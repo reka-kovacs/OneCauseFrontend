319573e62c61d8a7aa8b7202646d2e6616a1d6ac
reka-kovacs <61095901+reka-kovacs@users.noreply.github.com>
2022-05-28 17:16:12 -0400

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
+import { repeat } from 'rxjs';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +14,7 @@ export class AuthService {
   API_URL = 'http://localhost:8080';
   TOKEN_KEY = 'token';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, private snackBar: MatSnackBar) { }
 
   login(username: string, password: string, token: string) {
     const headers = {
@@ -26,10 +28,15 @@ export class AuthService {
     };
 
     this.http.post(this.API_URL+"/login", data, headers).subscribe(
-      (res: any) => {
-          localStorage.setItem(this.TOKEN_KEY, res.token);
-
+      (response) => {
+        if (response) {
+          // localStorage.setItem(this.TOKEN_KEY, res.token);
           window.location.href="http://onecause.com"
+        }
+      },
+      (err: any) => {
+        console.log(err);
+        this.snackBar.open('INVALID USERNAME / PASSWORD', 'X', { duration: 4000, panelClass: ['red-snackbar']});
       }
     );
   }
